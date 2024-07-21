d205ae2e0c8c1e0bd9be5b8be9b7f60f45b0c125
heyraeh <heyraeh@gmail.com>
2024-07-21 16:27:19 -0600

    Quartz sync: Jul 21, 2024, 4:27 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -4,7 +4,10 @@ import * as Component from "./quartz/components"
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
-  header: [],
+  header: [
+    Component.Search(),
+    Component.Darkmode(),
+  ],
   afterBody: [],
   footer: Component.Footer({
     links: {
