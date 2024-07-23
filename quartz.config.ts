707116a96c7e6004cead728b404ac948a557a8a6
heyraeh <heyraeh@gmail.com>
2024-07-23 11:14:00 -0600

    Quartz sync: Jul 23, 2024, 11:14 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "heyraeh's 🌱 digital garden",
+    pageTitle: "heyraeh's 🌱",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
